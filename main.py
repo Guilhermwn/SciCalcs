617815c6de2a793ab40fb5c01b6dc12909a2e896
Guilhermwn <limaoplaykglp@gmail.com>
2024-06-25 00:08:37 -0300

    test

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import uvicorn
 from os import getenv
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return {'hello': 'world'}
 
 if __name__ == '__main__':
     port = int(getenv('PORT', 8000))
-    uvicorn.run("backend:fast", host='0.0.0.0', port=port)
+    uvicorn.run("main:app", host='0.0.0.0', port=port)
+    # uvicorn.run("backend:fast", host='0.0.0.0', port=port)
